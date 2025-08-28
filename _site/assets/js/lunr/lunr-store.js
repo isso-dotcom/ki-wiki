f1f966a63c4bb567b4ba4028fb9f29de9e05c7b4
DominikHirsch <dominik.hirsch@gmx.net>
2025-08-28 14:09:53 +0200

    update gemfile

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Seite 1",
+        "excerpt":"Seite 1   Das ist Seite 1 im Wiki.  ","categories": [],
+        "tags": [],
+        "url": "/seite1/",
+        "teaser": null
+      },{
+        "title": "Seite 2",
+        "excerpt":"Seite 2   Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
+        "tags": [],
+        "url": "/seite2/",
+        "teaser": null
+      },{
+        "title": "Unterseite 1",
+        "excerpt":"Unterseite 1   Das ist die erste Unterseite.  ","categories": [],
+        "tags": [],
+        "url": "/seite2/sub1/",
+        "teaser": null
+      },{
+        "title": "Unterseite 2",
+        "excerpt":"Unterseite 2   Das ist die zweite Unterseite.  ","categories": [],
+        "tags": [],
+        "url": "/seite2/sub2/",
+        "teaser": null
+      },]
