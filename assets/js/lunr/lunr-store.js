d09c5590e4a81b2d0b6ba6f21fadbeb2adb0b9b0
isso-dotcom <isso-dotcom@users.noreply.github.com>
2025-10-30 17:31:28 +0000

    deploy: 2adb91cf6097099f6b1538a820ea195386db63ac

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Seite 1",
+        "excerpt":"Das ist Seite 1 im Wiki.  ","categories": [],
+        "tags": [],
+        "url": "/jekyll-wiki-template/seite1/",
+        "teaser": null
+      },{
+        "title": "Seite 2",
+        "excerpt":"Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
+        "tags": [],
+        "url": "/jekyll-wiki-template/seite2/",
+        "teaser": null
+      },{
+        "title": "Unterseite 1",
+        "excerpt":"Das ist die erste Unterseite von Seite 2.  ","categories": [],
+        "tags": [],
+        "url": "/jekyll-wiki-template/seite2/sub1/",
+        "teaser": null
+      },{
+        "title": "Unterseite 2",
+        "excerpt":"Das ist die zweite Unterseite von Seite 2.  ","categories": [],
+        "tags": [],
+        "url": "/jekyll-wiki-template/seite2/sub2/",
+        "teaser": null
+      },]
